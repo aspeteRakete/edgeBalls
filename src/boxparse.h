4642aeae31c00ed5b24ecb2f2f909ff9384f7183
Felix Dubrownik <fdubrownik@gmail.com>
2014-06-14 19:47:54 +0200

    changed Vertex to ofPoint

--- src/boxparse.h
+++ src/boxparse.h
@@ -4,9 +4,9 @@
 #include "ofxJSONElement.h"
 
 namespace parse {
-
-typedef std::pair<int, int> Vertex;
-typedef std::vector<Vertex> Polygon;
+const std::string edgeUrl = "192.168.1.00:8080/scene"
+//typedef std::pair<int, int> Vertex;
+typedef std::vector<ofPoint> Polygon;
 
 
 std::vector<Polygon> parsePolygons(const std::string &json);
