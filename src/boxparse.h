588edca53aacbc2ac1d3162af2144a7ceb6721b7
Felix Dubrownik <fdubrownik@gmail.com>
2014-06-14 19:50:45 +0200

    fehler behoben

--- src/boxparse.h
+++ src/boxparse.h
@@ -4,7 +4,7 @@
 #include "ofxJSONElement.h"
 
 namespace parse {
-const std::string edgeUrl = "192.168.1.00:8080/scene"
+const std::string edgeUrl = "192.168.1.00:8080/scene";
 //typedef std::pair<int, int> Vertex;
 typedef std::vector<ofPoint> Polygon;
 
